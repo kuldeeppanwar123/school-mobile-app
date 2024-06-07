904f043600e1f95d41b39fd0a7fb4c2f568f3e34
Kuldeep Panwar <kuldeeppanwar460@gmail.com>
2024-06-07 11:23:58 +0530

    folder structure complete

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text,TouchableOpacity} from 'react-native';
+import Login from './src/screens/Login';
+
+function App() {
+  return (
+  //  <Text className="text-red-700">Hello Kuldeep</Text>
+  <Login/>
+  );
+}
+
+export default App;
